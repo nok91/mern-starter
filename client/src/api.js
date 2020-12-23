67796c016f97b44ffaac05d9d0197131a9629558
Mohammed Nokri <mohammed.nokri@itg.co.uk>
2020-12-23 20:57:05 +0000

    Fix api url on client side

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://my-to-do-minimal.herokuapp.com/';
+const API_URL = 'https://my-to-do-minimal.herokuapp.com';
 
 async function createRoom(name) {
     try {
